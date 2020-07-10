3f3de060df47d69335e0b8421aa3fd3c2c6e99bc
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-07-10 14:31:49 +0900

    投稿確認機能作成

--- Rblog/app/controllers/blogs_controller.rb
+++ Rblog/app/controllers/blogs_controller.rb
@@ -5,4 +5,23 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+    redirect_to new_blog_path, notice: "投稿しました！"
+    else
+      render :new
+    end
+  end
+  def confirm
+   @blog = Blog.new(blog_params)
+   render :new if @blog.invalid?
+  end
+  private
+  def blog_params
+  params.require(:blog).permit(:content)
+  end
+  def find_blog
+    @blog = Blog.find(params[:id])
+  end
 end
